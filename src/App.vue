e0a16557a4cf7cf351d844179cc43304753d9e37
IgooLapTop2 <igoo@mail.ru>
2021-07-05 12:59:49 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <landing-section
+      :header="'Взгляните на процесс сборки своими глазами'"
+      :background="'#E5E5E5'"
+    >
+      <carousel-base />
+    </landing-section>
+
+    <landing-section>
+      <div class="placeholder-image"></div>
+
+      <!-- <generic-button
+        @click.native="showPopUp"
+        :text="'К покупкам'"
+      ></generic-button> -->
+      <generic-button @click.native="showPopUp">К покупкам</generic-button>
+    </landing-section>
+
+    <pop-up-window
+      v-if="isPopUpVisible"
+      @close="isPopUpVisible = false"
+      :visible="isPopUpVisible"
+    ></pop-up-window>
+
+    <div class="link-wrpper">
+      <a href="/src.zip">Source code</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  import carouselBase from "./components/carousel-base.vue";
+  import LandingSection from "./components/landing-section.vue";
+  import GenericButton from "./components/generic-button.vue";
+  import PopUpWindow from "./components/pop-up.vue";
+
+  export default {
+    name: "App",
+
+    components: {
+      carouselBase,
+      PopUpWindow,
+      LandingSection,
+      GenericButton,
+    },
+
+    data() {
+      return {
+        isPopUpVisible: false,
+      };
+    },
+
+    methods: {
+      showPopUp() {
+        this.isPopUpVisible = true;
+      },
+    },
+  };
+</script>
+
+<style>
+  .placeholder-image {
+    margin: 60px auto 50px;
+    width: 860px;
+    height: 220px;
+    background-image: url("assets/sale-banner.png");
+    background-size: 100%;
+    background-repeat: no-repeat;
+  }
+
+  .link-wrapper {
+    height: 50px;
+  }
+
+  #app {
+    font-family: RotondaC, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #26303b;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  @media screen and (max-width: 600px) {
+    .placeholder-image {
+      margin: 60px auto 50px;
+      width: 280px;
+      height: 522px;
+      background-image: url("assets/sale-banner-mobile.png");
+      background-size: 100%;
+      background-repeat: no-repeat;
+    }
+  }
+</style>
---
+++ src/components/generic-button.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <!-- <button class="generic-button" :disabled="disabled">{{ text }}</button> -->
+    <button class="generic-button" :disabled="disabled">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GenericButton",
+
+    props: {
+      // text: {
+      //   type: String,
+      //   required: false,
+      //   default: "Button",
+      // },
+      disabled: {
+        type: Boolean,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  button:disabled {
+    opacity: 0.6;
+    cursor: default;
+  }
+
+  .generic-button {
+    width: 340px;
+    height: 70px;
+    background: #7db945;
+    border: 0;
+    border-radius: 10px;
+    font-size: 22px;
+    line-height: 36px;
+    font-weight: 700;
+    letter-spacing: 0.01em;
+    text-align: center;
+    color: white;
+    display: block;
+    margin: 0 auto;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 600px) {
+    .generic-button {
+      width: 280px;
+      font-size: 20px;
+      line-height: 34px;
+    }
+  }
+</style>
